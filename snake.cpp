1015891d460d507222f41773e47861d37a9d420d
SUNMI <84156897+SUNMI-KIM@users.noreply.github.com>
2022-06-15 18:42:25 +0900

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -54,10 +54,7 @@ Snake::Snake(int x, int y)
 
 void Snake::showSnake()
 {
-    for (int i = 0; i < snake_length; i++) {
-        map[head_x][head_y++] = 3;
-    }
-    
+    map[head_y][head_x] = 3;
 }
 void color() {
     start_color();
@@ -75,81 +72,82 @@ int main() {
     border('@', '@', '@', '@', '@', '@', '@', '@');
     refresh();
 
-    WINDOW *gameBoard = newwin(30, 40, 2, 2);
+    WINDOW* gameBoard = newwin(30, 40, 2, 2);
     keypad(gameBoard, TRUE);
     WINDOW* scoreBoard = newwin(14, 36, 3, 43);
     WINDOW* missionBoard = newwin(14, 36, 18, 43);
     Snake MainSnake = Snake(15, 20); // Snake head 초기에 가장 중앙에 배치함
     while (1) {
-           
-            MainSnake.showSnake();           // 변경사항 생길 때마다 실행해야함
-            int key;
-            noecho();
-            key = getch();
-            switch (key) {
-            case(0402): // KEY_DOWN
-                MainSnake.head_x += 1;
-                break;
-            case(0403): // KEY_UP
-                MainSnake.head_x -= 1;
-                break;
-            case(0404): // KEY_LEFT
-                MainSnake.head_y -= 1;
-                break;
-            case(0405): // KEY_RIGHT
-                MainSnake.head_y += 1;
-                break;
-            }
-            start_color();
-            init_pair(1, COLOR_GREEN, COLOR_BLACK);
-            attron(COLOR_PAIR(1));
-            attroff(COLOR_PAIR(1));
-            refresh();
 
-            for (int i = 0; i < 30; i++) {
-                for (int j = 0; j < 40; j++) {
-                    switch (MainSnake.map[i][j]) {
-                    case 0:
-                        mvwprintw(gameBoard, i, j, " ");
-                        break;
-                    case 1:
-                        mvwprintw(gameBoard, i, j, "#");
-                        break;
-                    case 3:
-                        mvwprintw(gameBoard, i, j, "*");
-                        break;
-                    case 4:
-                        mvwprintw(gameBoard, i, j, "@");
-                        break;
-                    }
-                }
-                wrefresh(gameBoard);
-            }
-
-            init_pair(3, COLOR_BLACK, COLOR_WHITE);
-            wmove(scoreBoard, 0, 0);
-            wattron(scoreBoard, COLOR_PAIR(3));
-            wborder(scoreBoard, '|', '|', '-', '-', '*', '*', '*', '*');
-            wbkgd(scoreBoard, COLOR_PAIR(3));
-            mvwprintw(scoreBoard, 3, 3, "body: %d", MainSnake.snake_length);
-            mvwprintw(scoreBoard, 4, 3, "grow: %d", MainSnake.snake_length);
-            mvwprintw(scoreBoard, 5, 3, "poison: %d", MainSnake.snake_length);
-            mvwprintw(scoreBoard, 6, 3, "gate: %d", MainSnake.snake_length);
-            wrefresh(scoreBoard);
-
-            wbkgd(missionBoard, COLOR_PAIR(6));
-            init_pair(3, COLOR_BLACK, COLOR_WHITE);
-            wmove(missionBoard, 0, 0);
-            wattron(missionBoard, COLOR_PAIR(3));
-            wborder(missionBoard, '|', '|', '-', '-', '*', '*', '*', '*');
-            wbkgd(missionBoard, COLOR_PAIR(3));
-            mvwprintw(missionBoard, 3, 3, "body: %d", MainSnake.snake_length);
-            mvwprintw(missionBoard, 4, 3, "grow: %d", MainSnake.snake_length);
-            mvwprintw(missionBoard, 5, 3, "poison: %d", MainSnake.snake_length);
-            mvwprintw(missionBoard, 6, 3, "gate: %d", MainSnake.snake_length);
-            wrefresh(missionBoard);
+                 // 변경사항 생길 때마다 실행해야함
+        int key;
+        noecho();
+        key = getch();
+        switch (key) {
+        case(KEY_DOWN): // KEY_DOWN
+            MainSnake.head_x += 1;
+            break;
+        case(KEY_UP): // KEY_UP
+            MainSnake.head_x -= 1;
+            break;
+        case(KEY_LEFT): // KEY_LEFT
+            MainSnake.head_y -= 1;
+            break;
+        case(KEY_RIGHT): // KEY_RIGHT
+            MainSnake.head_y += 1;
+            break;
         }
-    
+        MainSnake.showSnake();  
+        start_color();
+        init_pair(1, COLOR_GREEN, COLOR_BLACK);
+        attron(COLOR_PAIR(1));
+        attroff(COLOR_PAIR(1));
+        refresh();
+
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 40; j++) {
+                switch (MainSnake.map[i][j]) {
+                case 0:
+                    mvwprintw(gameBoard, i, j, " ");
+                    break;
+                case 1:
+                    mvwprintw(gameBoard, i, j, "#");
+                    break;
+                case 3:
+                    mvwprintw(gameBoard, i, j, "*");
+                    break;
+                case 4:
+                    mvwprintw(gameBoard, i, j, "@");
+                    break;
+                }
+            }
+            wrefresh(gameBoard);
+        }
+
+        init_pair(3, COLOR_BLACK, COLOR_WHITE);
+        wmove(scoreBoard, 0, 0);
+        wattron(scoreBoard, COLOR_PAIR(3));
+        wborder(scoreBoard, '|', '|', '-', '-', '*', '*', '*', '*');
+        wbkgd(scoreBoard, COLOR_PAIR(3));
+        mvwprintw(scoreBoard, 3, 3, "body: %d", MainSnake.snake_length);
+        mvwprintw(scoreBoard, 4, 3, "grow: %d", MainSnake.snake_length);
+        mvwprintw(scoreBoard, 5, 3, "poison: %d", MainSnake.snake_length);
+        mvwprintw(scoreBoard, 6, 3, "gate: %d", MainSnake.snake_length);
+        wrefresh(scoreBoard);
+
+        wbkgd(missionBoard, COLOR_PAIR(6));
+        init_pair(3, COLOR_BLACK, COLOR_WHITE);
+        wmove(missionBoard, 0, 0);
+        wattron(missionBoard, COLOR_PAIR(3));
+        wborder(missionBoard, '|', '|', '-', '-', '*', '*', '*', '*');
+        wbkgd(missionBoard, COLOR_PAIR(3));
+        mvwprintw(missionBoard, 3, 3, "body: %d", MainSnake.snake_length);
+        mvwprintw(missionBoard, 4, 3, "grow: %d", MainSnake.snake_length);
+        mvwprintw(missionBoard, 5, 3, "poison: %d", MainSnake.snake_length);
+        mvwprintw(missionBoard, 6, 3, "gate: %d", MainSnake.snake_length);
+        wrefresh(missionBoard);
+    }
+
     endwin();
     return 0;
 
